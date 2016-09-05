7058030ce516ee90765c2cb9a24f44f1e9d91b3e
jimhshen <jim.h.shen@gmail.com>
2016-09-04 22:30:35 -0700

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -14,7 +14,7 @@ download.file(fileurl, file.path(path, file))
 #Extract the file
 unzip(zipfile="./Dataset.zip",exdir="./data")
 
-#Set File Location
+#Set file path
 fileLocation <- file.path("./data", "UCI HAR Dataset")
 
 #Get data
@@ -24,7 +24,7 @@ dataSubjectTest  <- fread(file.path(fileLocation, "test" , "subject_test.txt" ))
 dataActivityTrain <- fread(file.path(fileLocation, "train", "Y_train.txt"))
 dataActivityTest  <- fread(file.path(fileLocation, "test" , "Y_test.txt" ))
 
-tableTrain <- read.table(file.patdatah(fileLocation, "train", "X_train.txt"))
+tableTrain <- read.table(file.path(fileLocation, "train", "X_train.txt"))
 tableTest <- read.table(file.path(fileLocation, "test" , "X_test.txt" ))
 dataFeaturesTrain <-data.table(tableTrain)
 dataFeaturesTest <-data.table(tableTest)
@@ -37,7 +37,7 @@ setnames(dataSubject, "V1", "subject")
 dataActivity <- rbind(dataActivityTrain, dataActivityTest)
 setnames(dataActivity, "V1", "activity")
 
-dataFeat<- rbind(dataFeaturesTrain, dataFeaturesTest)
+dataFeatures <- rbind(dataFeaturesTrain, dataFeaturesTest)
 featuresNames <- fread(file.path(fileLocation, "features.txt"))
 names(dataFeatures) <- featuresNames$V2
 
